7f08ace1e79b8045b5d6f1f7fad2aa2144996ac8
xiomara052 <xbautista052@gmail.com>
2024-10-28 22:21:49 -0500

    JavaScript
    arregle e integre nuevas funciones

--- js/gastos.js
+++ js/gastos.js
@@ -11,8 +11,6 @@ function eventListener(){
 }
 
 
-
-
 //crear la clase principal
 class Presupuesto
 {
@@ -24,32 +22,57 @@ class Presupuesto
     
     }
 
-    nuevoGasto (gastos){
-        this.gastos = []
-        this.calcularRestante();
+    nuevoGasto(gasto){
+        this.gastos =  [...this.gastos, gasto];
+        this.calcularRestante()
+
     }
 
+    calcularRestante(){
+        
+    }
 }
 //clase que maneja la interfaz de usuario
 class UI
 {
     insertarpresupuesto(cantidad){
         document.querySelector('#total').textContent = cantidad.presupuesto;
-        document.querySelector('#restante').textContent  = cantidad.restante;
+        document.querySelector('#restante').textContent  = cantidad.presupuesto;
 
     }
-    imprimirAlerta(){
+    ImprimirAlerta(mensaje,tipo){
 
         //crear el div
-        const divmensaje = document.createElement('div')
-        divmensaje.classList('text-center','alert');
+        const divmensaje  = document.createElement('div');
+        divmensaje.classList.add('text-center','alert');
 
-    }
-}
+        //si es de tipo error se debe agregar a la clase
+        if(tipo === 'error'){
+            divmensaje.classList.add('alert-danger');
+        }else{
+            divmensaje.classList.add('alert-primary');
+        }
+        //mensaje de error
+        divmensaje.textContent = mensaje;
+        //inseertar en el DOM
+        const contenidoPrincipal = document.querySelector('.contenido-gastos');
+        contenidoPrincipal.insertBefore(divmensaje,formulario)
+
+        //programar el tiempo que dura la alerta
+        setTimeout(() => {
+            document.querySelector('.contenido-principal .alert').remove();
+        },3000);
+        
+ }
+
 //insertar el gasto en la lista
-agregarGastolistado(gastos)
+agregarGastoListado(gasto){
 
-//crear un objeto de la clase ui
+}
+
+}
+
+//crear un objeeto de la clase ui
 const ui = new UI();
 let presupuesto;
 
@@ -60,9 +83,7 @@ function preguntarpresupuesto(){
     //validar lo  ingresado por el usuario
     if (valorpre === '' || isNaN(valorpre) || valorpre === null || valorpre <= 0) 
         {
-
             window.location.reload();
-    
         }
         //presupuesto es valido
         presupuesto  = new Presupuesto(valorpre);
@@ -71,21 +92,22 @@ function preguntarpresupuesto(){
     //mostrar en el html el valor del presupuesto ingresado
     ui.insertarpresupuesto(presupuesto);
 }
+//leer lo registrado en el formulario
+function agregarGasto(e){
+    e.preventDefault();
 
-function agregarGasto (){
-    eventListener.preventDefault()
-    
-    //definir las variables del formulario 
-    const nombre = document.querySelector('#').value;
-    const valor = Number(document.querySelector('#').value);
+    //definir las variables del formulario
+     const Nombre  = document.querySelector('#gasto').value;
+     const Valor = Number(document.querySelector('#cantidad').value);
 
-    //validar los campos el formulario
-    if (nombre == '' || valor == ''){
+     //validar los campos del formulario
+     if (Nombre === '' || Valor === ''){
 
-        ui.imprimirAlerta('Ambos campos son obligatorios'), 'error';
+        ui.ImprimirAlerta('Ambos campos son  obligatorios', 'error');
         //return;
-    }else if(valor < 8 || isNaN(valor)){
-        ui.imprimirAlerta('El valor no es correcto',error);
-    }
-
+     
+     }else if(Valor < 0 || isNaN(Valor)){
+        ui.ImprimirAlerta('El valor del gasto no puede ser negativo', 'error');
+     }
+     
 }
